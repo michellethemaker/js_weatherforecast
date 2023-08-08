a77cbc422e957f844a21cc100a782bc2225d0af0
michellethemaker <limmm.michelle@gmail.com>
2023-08-08 17:30:48 +0800

    updated

--- weather/script.js
+++ weather/script.js
@@ -1,64 +1,79 @@
 class WeatherData{
     constructor(){
         this.data = null;
-        this.area = null;
-        this.forecast = null;
+        this.stations = null;
+        this.temp = null;
         this.latitude = null;
         this.longitude = null;
     }
     
-    async fetchData(testdata){
+    async fetchData(){
     try {
-      const response = await fetch('https://api.data.gov.sg/v1/environment/2-hour-weather-forecast') // API URL
+
+      // clear previous labels first
+      const clearLabels = document.querySelectorAll('.label');
+      clearLabels.forEach(label=>{
+        label.remove();
+        console.log("removed");
+      });
+
+      const response = await fetch('https://api.data.gov.sg/v1/environment/air-temperature') // API URL
       const data = await response.json()
       
       this.data = data;
       // // Process the API response
       const weatherData = data; // Assuming the API response has a 'weather' key
-      for (const forecast of weatherData.items[0].forecasts) {
-          if (forecast.area === testdata) {
-          console.log(`Successful data fetch. Weather forecast for ${forecast.area}: ${forecast.forecast}`);
-          this.area = forecast.area;
-          this.forecast = forecast.forecast;
-          const coords = weatherData.area_metadata.find((area) => area.name === this.area);
-          this.latitude = coords.label_location.latitude;
-          this.longitude = coords.label_location.longitude;
-          console.log("coords: ", this.latitude, this.longitude);
-          break;
-          }
-          else{
-            // Cancel out forecast
-            this.area = null;
-            this.forecast = null;
-          }
+      console.log(weatherData.metadata.stations);
+      console.log("!!!");
+      console.log(weatherData.items[0].readings[0]);
+      for (const stations of weatherData.metadata.stations) {
+        console.log(stations)
+        const temp = weatherData.items[0].readings.find(stationtemp =>stationtemp.station_id === stations.device_id);
+        console.log(`${this.stations} temp is:  ${temp.value}`);
+        console.log(stations.id, stations.name, stations.location.latitude, stations.location.longitude);
+          // console.log(`Successful data fetch. Weather stations for ${stations.id}: ${stations.name}`);
+          this.data = stations.id;
+          this.temp = temp.value;
+          this.stations = stations.name;
+          this.latitude = stations.location.latitude;
+          this.longitude = stations.location.longitude;
+          const pixelX = scale(this.longitude, 103.635, 103.987, 0,430);
+          const pixelY = scale(this.latitude, 1.205926, 1.445, 280, 0); 
+          
+          // Create label for each station
+          const label = document.createElement("div");
+          label.className = "label";
+          label.innerText = this.stations;
+          label.title = this.temp;
+          label.style.left = pixelX + 'px';
+          label.style.top = pixelY + 'px';  
+          label.style.display = 'block';
+          labels.appendChild(label);
       }
-      }
+    }
  
     catch (error) {
     console.error('Error fetching data:', error);
     throw error;
     }
   }
-  
-    fetchForecast(){
-      return this.forecast;
-    }
-
+   
     displayPin() {
       // Get the map image element and the pin container element
       const pin = document.getElementById('pin');
       const lat = this.latitude;
       const long = this.longitude;
       
+      
       // note that x = based on long, y = based on lat. 
       // Y is inverted as coords=cartesian, png is normal
       // not the most accurate, esp as coords are on a spherical map and adjustments must be made
-      const pixelX = scale(long, 103.635, 103.987, 0,900);
-      const pixelY = scale(lat, 1.205926, 1.445, 600, 0); 
+      const pixelX = scale(long, 103.635, 103.987, 0,430);
+      const pixelY = scale(lat, 1.205926, 1.445, 280, 0); 
 
-      // Find the area data in the API response
-      console.log(`Latitude for ${this.area}: `+ lat + ` i.e. ${pixelY}`);
-      console.log(`Longitude for ${this.area}: ` + long + ` i.e. ${pixelX}`);
+      // Find the stations data in the API response
+      console.log(`Latitude for ${this.stations}: `+ lat + ` i.e. ${pixelY}`);
+      console.log(`Longitude for ${this.stations}: ` + long + ` i.e. ${pixelX}`);
     
       // Set the position of the pin using absolute positioning
       pin.style.left = pixelX + 'px';
@@ -72,23 +87,14 @@ class WeatherData{
 
 const weatherDataInstance = new WeatherData();
 
+const fetchInterval = setInterval(weatherDataInstance.fetchData, 30000);
+
 async function addItem() {
-  var userInput = document.getElementById("location").value;
-  // if userInput === data.items[0].forecasts, accept
+  var userInput = document.getElementById("textbox").value;
+  // if userInput === data.items[0].stationss, accept
           
   if (userInput.trim() !== ""){
-  console.log(weatherDataInstance.fetchData(userInput));
-  await weatherDataInstance.fetchData(userInput);
-  const forecast = weatherDataInstance.fetchForecast();
-  if (forecast !== null){
-  weatherInfo.textContent = "Current forecast for "+ userInput + ": " + weatherDataInstance.fetchForecast();
-  weatherDataInstance.displayPin();
-      }
-  else{
-    weatherInfo.textContent = "Location not found!";
-    const pin = document.getElementById("pin");
-    pin.style.display = "none"
-  }
+    await weatherDataInstance.fetchData();
     }
   }
 
@@ -108,7 +114,7 @@ function alertTime(){
   const weatherInfo = document.getElementById("weatherInfo")
 
   //add event listener for text box
-  const searchBox = document.getElementById("location");
+  const searchBox = document.getElementById("textbox");
   searchBox.addEventListener("keypress", function(event){
     if (event.key === "Enter"){
         event.preventDefault(); //required to prevent reset of webpage
@@ -117,6 +123,11 @@ function alertTime(){
     }
   });
 
+  const labels = document.getElementById("labels");
+
   function scale (number, inMin, inMax, outMin, outMax){
     return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
   }
+
+
+
